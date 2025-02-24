d3ede676f09ef0c465ea9fca24aa2412bee5558b
lida-sh <l_shafiei_2005@yahoo.com>
2025-02-24 12:33:22 +0330

    fix reflect metadata test

--- composables/architectures/architecture.dto.ts
+++ composables/architectures/architecture.dto.ts
@@ -1,59 +1,59 @@
-import { Expose, Type } from "class-transformer";
-import "reflect-metadata";
-import { FileDto,} from "../file.dto";
+// import { Expose, Type } from "class-transformer";
+// import "reflect-metadata";
+// import { FileDto,} from "../file.dto";
 
-import { ProcessTreeStructDto } from "../processes/process.dto";
-export class ArchitectureBaseDto {
-  @Expose()
-  id: number;
-  @Expose()
-  title: string;
-  @Expose({toClassOnly: true})
-  get displayTitle(){
-    // if (this.type === "assistance") {
-    //   return "معاونت " + this.title;
-    // } else if (this.type === "administration") {
-      return "اداره کل " + this.title;
-    // }
-  }
-  set displayTitle(value){
+// import { ProcessTreeStructDto } from "../processes/process.dto";
+// export class ArchitectureBaseDto {
+//   @Expose()
+//   id: number;
+//   @Expose()
+//   title: string;
+//   @Expose({toClassOnly: true})
+//   get displayTitle(){
+//     if (this.type === "assistance") {
+//       return "معاونت " + this.title;
+//     } else if (this.type === "administration") {
+//       return "اداره کل " + this.title;
+//     }
+//   }
+//   set displayTitle(value){
 
-  }
-  @Expose()
-  code: string;
-  @Expose()
-  type: string;
-}
-export class ArchitectureClientBaseDto {
-  @Expose()
-  title: string;
-  @Expose({toClassOnly: true})
-  get displayTitle(){
-    // if (this.type === "assistance") {
-    //   return "معاونت " + this.title;
-    // } else if (this.type === "administration") {
-      return "اداره کل " + this.title;
-    // }
-  }
-  set displayTitle(value){
+//   }
+//   @Expose()
+//   code: string;
+//   @Expose()
+//   type: string;
+// }
+// export class ArchitectureClientBaseDto {
+//   @Expose()
+//   title: string;
+//   @Expose({toClassOnly: true})
+//   get displayTitle(){
+//     if (this.type === "assistance") {
+//       return "معاونت " + this.title;
+//     } else if (this.type === "administration") {
+//       return "اداره کل " + this.title;
+//     }
+//   }
+//   set displayTitle(value){
 
-  }
-  @Expose()
-  type: string;
-}
-export class ArchitectureDto extends ArchitectureBaseDto {
-  @Expose()
-  slug: string;
-  @Expose()
-  @Type(()=>FileDto)
-  files: FileDto[];
-  @Expose()
-  description: string;
-  @Expose()
-  created_at: string;
-}
-export class ArchitectureTreeStructDto extends ArchitectureBaseDto{
-  @Expose()
-  @Type(()=>ProcessTreeStructDto)
-  processes: ProcessTreeStructDto[]
-}
+//   }
+//   @Expose()
+//   type: string;
+// }
+// export class ArchitectureDto extends ArchitectureBaseDto {
+//   @Expose()
+//   slug: string;
+//   @Expose()
+//   @Type(()=>FileDto)
+//   files: FileDto[];
+//   @Expose()
+//   description: string;
+//   @Expose()
+//   created_at: string;
+// }
+// export class ArchitectureTreeStructDto extends ArchitectureBaseDto{
+//   @Expose()
+//   @Type(()=>ProcessTreeStructDto)
+//   processes: ProcessTreeStructDto[]
+// }
